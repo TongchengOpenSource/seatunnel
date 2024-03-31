455144b495c4eb5458185972321bf27e43eb5f0d
ClownXC <chen3494269@163.com>
2024-03-31 13:10:13 +0800

    Optimize the logic of getSeaTunnelServer()

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/RestHttpGetCommandProcessor.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/RestHttpGetCommandProcessor.java
@@ -374,17 +374,6 @@ public class RestHttpGetCommandProcessor extends HttpCommandProcessor<HttpGetCom
                                         .getSerializationService()
                                         .toObject(jobInfo.getJobImmutableInformation()));
 
-        ClassLoaderService classLoaderService = getSeaTunnelServer(false).getClassLoaderService();
-        ClassLoader classLoader =
-                classLoaderService.getClassLoader(
-                        jobId, jobImmutableInformation.getPluginJarsUrls());
-        LogicalDag logicalDag =
-                CustomClassLoadedObject.deserializeWithCustomClassLoader(
-                        this.textCommandService.getNode().getNodeEngine().getSerializationService(),
-                        classLoader,
-                        jobImmutableInformation.getLogicalDag());
-        classLoaderService.releaseClassLoader(jobId, jobImmutableInformation.getPluginJarsUrls());
-
         SeaTunnelServer seaTunnelServer = getSeaTunnelServer(true);
         String jobMetrics;
         JobStatus jobStatus;
@@ -401,11 +390,24 @@ public class RestHttpGetCommandProcessor extends HttpCommandProcessor<HttpGetCom
                                                     getNode().nodeEngine,
                                                     new GetJobStatusOperation(jobId))
                                             .join()];
+            seaTunnelServer = getSeaTunnelServer(false);
+
         } else {
             jobMetrics =
                     seaTunnelServer.getCoordinatorService().getJobMetrics(jobId).toJsonString();
             jobStatus = seaTunnelServer.getCoordinatorService().getJobStatus(jobId);
         }
+
+        ClassLoaderService classLoaderService = seaTunnelServer.getClassLoaderService();
+        ClassLoader classLoader =
+                classLoaderService.getClassLoader(
+                        jobId, jobImmutableInformation.getPluginJarsUrls());
+        LogicalDag logicalDag =
+                CustomClassLoadedObject.deserializeWithCustomClassLoader(
+                        this.textCommandService.getNode().getNodeEngine().getSerializationService(),
+                        classLoader,
+                        jobImmutableInformation.getLogicalDag());
+        classLoaderService.releaseClassLoader(jobId, jobImmutableInformation.getPluginJarsUrls());
 
         jobInfoJson
                 .add(RestConstant.JOB_ID, String.valueOf(jobId))
