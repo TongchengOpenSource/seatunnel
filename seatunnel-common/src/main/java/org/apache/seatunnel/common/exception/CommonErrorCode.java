fbd3a7ce3b2120d3c9be7db36589143c667493db
ClownXC <598457447@qq.com>
2025-02-28 08:48:34 +0800

    [Feature][Connector-v2]  update common error code

--- seatunnel-common/src/main/java/org/apache/seatunnel/common/exception/CommonErrorCode.java
+++ seatunnel-common/src/main/java/org/apache/seatunnel/common/exception/CommonErrorCode.java
@@ -22,7 +22,6 @@ public enum CommonErrorCode implements SeaTunnelErrorCode {
     FILE_OPERATION_FAILED("COMMON-01", "<identifier> <operation> file '<fileName>' failed."),
     JSON_OPERATION_FAILED(
             "COMMON-02", "<identifier> JSON convert/parse '<payload>' operation failed."),
-    //    ILLEGAL_ARGUMENT("COMMON-06", "'<argument>' is an illegal argument for '<identifier>'. "),
     ILLEGAL_ARGUMENT("COMMON-06", "illegal argument '<argument>' of '<operation>'"),
     UNSUPPORTED_DATA_TYPE(
             "COMMON-07", "'<identifier>' unsupported data type '<dataType>' of '<field>'"),
